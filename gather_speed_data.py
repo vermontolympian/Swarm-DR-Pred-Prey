7ae9754dd023d19e27d1b607c23824dc04605b9a
VermontOlympian <2boog25@gmail.com>
2023-12-06 20:26:04 -0500

    Genreated Final Speed Graphs

--- gather_speed_data.py
+++ gather_speed_data.py
@@ -21,9 +21,9 @@ vis_params = {
     "animation_codec": "h264"
 }
 
+
 def norm(array, axis=0):
     return np.sqrt(np.sum(np.power(array, 2), axis=axis))
-
 
 
 if __name__ == "__main__":
@@ -33,13 +33,14 @@ if __name__ == "__main__":
         writer.writeheader()
         permutations = []
         for M in [2, 3, 4, 5]:
-            for d, e in [(0.5, 0.4), (0.5, 0.5), (0.5, 0.6), (0.5, 0.7), (0.5, 0.8), (0.5, 0.9), (0.5, 1.0), (0.5, 1.1)]:
+            for d, e in [(0.5, 0.4), (0.5, 0.5), (0.5, 0.6), (0.5, 0.7), (0.5, 0.8), (0.5, 0.9), (0.5, 1.0),
+                         (0.5, 1.1)]:
                 for c in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
                     permutations.append((M, d, e, c))
 
         # for M in [2]:
         #     for d, e in [(0.5, 0.4)]:
-        #         for c in [0.1, 1]:
+        #         for c in [0.1]:
         #             permutations.append((M, d, e, c))
 
         avg_time = 0
@@ -53,12 +54,12 @@ if __name__ == "__main__":
             np.random.seed(0)
             vis_params['animation_path'] = f"media/DRTest6/Replication_M{M}_c{c}_d{d}_e{e}.mp4"
             caught, all_prey_v = run_model(
-                a=1,        # Prey-Prey Linear Long-Range Attraction Gain
-                b=0.2,      # Prey-Predator Repulsion Strength
-                c=c,      # Pred-Prey Attraction Strength
-                d=d,      # Pred-Pred Exponential Short Range Repulsion
-                e=e,        # Pred-Pred Linear Long-Range Attraction Gain
-                p=3,        # Power law for Predator-Prey Interactions
+                a=1,  # Prey-Prey Linear Long-Range Attraction Gain
+                b=0.2,  # Prey-Predator Repulsion Strength
+                c=c,  # Pred-Prey Attraction Strength
+                d=d,  # Pred-Pred Exponential Short Range Repulsion
+                e=e,  # Pred-Pred Linear Long-Range Attraction Gain
+                p=3,  # Power law for Predator-Prey Interactions
                 N=400,
                 M=M,
                 sim_params=sim_params, vis_params=vis_params
@@ -77,19 +78,30 @@ if __name__ == "__main__":
             vel = norm(all_prey_v[:, 0, :], axis=1)
             ang = np.arctan2(all_prey_v[:, 0, 1], all_prey_v[:, 0, 0])
 
+            colors = np.arange(len(vel))
             fig, ax = plt.subplots(figsize=(9, 6))
-            ax.scatter(vel, ang)
-            plt.title(f"M{M}_c{c}_d{d}_e{e}")
+            scatter = ax.scatter(vel, ang, c=colors, cmap='rainbow')
+
+            # Set title and colorbar
+            plt.title("         Speed Behaviors", fontsize=16, y=1.05)
+            plt.suptitle(f"M{M}_c{c}_d{d}_e{e}       ", fontsize=12, y=0.92)
+            plt.colorbar(scatter, label='Timestep')
+
+            # Set x-axis to log scale
             ax.set_xscale("log")
+
+            # Set labels for axes
             plt.xlabel('Linear Velocity')
             plt.ylabel('Angular Position (Rad)')
-            plt.savefig(f"SpeedGraphsLog/M{M}_c{c}_d{d}_e{e}.png", dpi=200)
+
+            # Show and save the plot
+            plt.savefig(f"SpeedGraphsFinal/M{M}_c{c}_d{d}_e{e}.png", dpi=200)
             # plt.show()
 
             this_time = int(time.time() - start_time)
-            avg_time = int((avg_time*i/(i+1)) + (this_time/(i+1)))
-            time_elapsed = int(time.time()-run_start)
-            time_remaining = int((len(permutations)-(i+1))*avg_time)
+            avg_time = int((avg_time * i / (i + 1)) + (this_time / (i + 1)))
+            time_elapsed = int(time.time() - run_start)
+            time_remaining = int((len(permutations) - (i + 1)) * avg_time)
             print(f"Timer: ")
             print(f"\tthis iter      = {datetime.timedelta(seconds=this_time)},")
             print(f"\tavg iter       = {datetime.timedelta(seconds=avg_time)},")
